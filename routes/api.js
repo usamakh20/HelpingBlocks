8b212037bfc39d631e2972f7247f7be631c01dc8
USAMA KHALID <usamathescientist@gmail.com>
2019-04-17 20:10:12 +0500

    Changes to Donor Api

--- routes/api.js
+++ routes/api.js
@@ -5,19 +5,8 @@ const jwt = require('jsonwebtoken');
 const user_middleware = require('../middleware/user');
 const contracts = require('../Contracts');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index',{title:'Helping Blocks'});
-});
-
-router.get('/track',user_middleware.api_auth,function(req,res){
-
-    contracts.DonationToken.methods.donations(0).call()
-        .then(result => {
-            return res.status(200).send({message: 'success', result: JSON.stringify(result)});
-        }).catch(error => {
-        return res.status(500).send({message: 'error'});
-    })
+router.get('/testToken',user_middleware.api_auth,function(req,res){
+    return res.status(200).send({message:'success'});
 });
 
 router.post('/register',function (req,res) {
@@ -59,24 +48,14 @@ router.post('/donor/login',function (req,res) {
     })
 });
 
+router.get('/track',user_middleware.api_auth,function(req,res){
 
-router.post('/shopkeeper/login',user_middleware.login,function (req,res) {
-    if (req.middleware.error)
-        return res.status(500).send({error: req.middleware.error});
-
-    else {
-        const token = jwt.sign({
-            userId: req.body.CNIC,
-            client_token: req.middleware.message.client_token
-        }, process.env.PWD, {
-            expiresIn: "1h"
-        });
-        return res.status(200).send({message: "Auth Successful", token: token});
-    }
-});
-
-router.get('/testToken',user_middleware.api_auth,function(req,res){
-    return res.status(200).send({message:'success'});
+    contracts.DonationToken.methods.donations(0).call()
+        .then(result => {
+            return res.status(200).send({message: 'success', result: JSON.stringify(result)});
+        }).catch(error => {
+        return res.status(500).send({message: 'error'});
+    })
 });
 
 
@@ -97,26 +76,19 @@ router.post('/donation',user_middleware.api_auth,function (req,res) {
 });
 
 router.get('/donation',user_middleware.api_auth,function(req,res){
-    //Todo: Retrieve donations from parity using web3
 
-    // user.findOne({_id:req.UserData.userId},function(err,user) {
-    //     if (err)
-    //         return res.status(500).send({message: 'Server Error'});
-    //
-    //     else return res.status(200).send({donation_ids: user.donations});
-    //
-    // });
+    return res.status(200).send({message:'success', donations: req.UserData.Donor.donations});
 
-    return res.status(200).send({message:'success', donations: [
-            {id:"242341",date:"Monday, Apr 23, 2019",amount:45},
-            {id:"337567",date:"Tuesday, Feb 18, 2019",amount:2638},
-            {id:"4324",date:"Friday, May 12, 2019",amount:863},
-            {id:"312",date:"Wednesday, July 2, 2019",amount:3754},
-            {id:"4234244",date:"Thursday, June 9, 2019",amount:43},
-            {id:"65",date:"Sunday, Sept 16, 2019",amount:454978},
-            {id:"26545632",date:"Friday, Oct 1, 2019",amount:79449},
-            {id:"346563665",date:"Saturday, Dec 5, 2019",amount:46236}
-        ]});
+        //     [
+        //     {id:"242341",date:"Monday, Apr 23, 2019",amount:45},
+        //     {id:"337567",date:"Tuesday, Feb 18, 2019",amount:2638},
+        //     {id:"4324",date:"Friday, May 12, 2019",amount:863},
+        //     {id:"312",date:"Wednesday, July 2, 2019",amount:3754},
+        //     {id:"4234244",date:"Thursday, June 9, 2019",amount:43},
+        //     {id:"65",date:"Sunday, Sept 16, 2019",amount:454978},
+        //     {id:"26545632",date:"Friday, Oct 1, 2019",amount:79449},
+        //     {id:"346563665",date:"Saturday, Dec 5, 2019",amount:46236}
+        // ]
 });
 
 router.get('/donation/:id',user_middleware.api_auth,function(req,res){
@@ -130,6 +102,21 @@ router.get('/donation/:id',user_middleware.api_auth,function(req,res){
     //     timestamp:1555597802,
     //     signer:{id:"0x56567af4", name:"Umer"},
 
+});
+
+router.post('/shopkeeper/login',user_middleware.login,function (req,res) {
+    if (req.middleware.error)
+        return res.status(500).send({error: req.middleware.error});
+
+    else {
+        const token = jwt.sign({
+            userId: req.body.CNIC,
+            client_token: req.middleware.message.client_token
+        }, process.env.PWD, {
+            expiresIn: "1h"
+        });
+        return res.status(200).send({message: "Auth Successful", token: token});
+    }
 });
 
 router.post('/pay',user_middleware.api_auth,function (req,res) {
